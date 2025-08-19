749cfd57ddac0bbdf39f655fddedfc2c64baae37
Brad Presson <bradpresson@aol.com>
2025-08-19 15:23:42 -0400

    Answered question 2. Downloaded the natural gas dataset. Listed the date that the data was downloaded.

--- scripts/quiz1.R
+++ scripts/quiz1.R
@@ -1,6 +1,7 @@
 ## Getting and Cleaning Data Module 1 Quiz ##
 
 library(dplyr)
+if(!file.exists("data")){dir.create("data")}
 
 # Download a file from the web #
 
@@ -8,14 +9,27 @@ fileURL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv" # 
 setwd("C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data") # set working directory
 download.file(fileURL,destfile = "C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data/housing.csv") # established path and named new .csv file in working directory
 list.files("C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data") # check that the new file now exists in the specified directory.
-dateDownloaded <- date() # specify when the data was downloaded. The dataset could change in the future, leading to different results.
-dateDownloaded
+dateDownloaded.housing <- date() # specify when the data was downloaded. The dataset could change in the future, leading to different results.
+dateDownloaded.housing
 
 # Read the housing file in the current working directory #
 
 housing.data<-read.table("C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data/housing.csv", sep = ",", header = TRUE, quote="")
 
-# Properties worth over $1,000,000 # 
+# QUESTION 1: Properties worth over $1,000,000 # 
 # Property Value variable = VAL in metadata
 
-sum(housing.data$VAL == 24, na.rm = TRUE) # turns the intergers into logical values (1 or 0), then adds. NA values are excluded.
+sum(housing.data$VAL == 24, na.rm = TRUE) # turns the integers into logical values (1 or 0), then adds. NA values are excluded.
+
+# QUESTION 2: Use the data you loaded from Question 1. Consider the variable FES in the code book. Which of the “tidy data” principles does this variable violate? #
+
+print("Tidy data has one variable per column; FES violates this") # segmentation, like "in LF" and "not in LF," are their own seperate variables.
+# The total number of variables could be consolidated by this efficiency.
+
+# QUESTION 3: Download the Excel spreadsheet on Natural Gas Acquisition Program #
+
+fileURL2<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(fileURL2,destfile = "C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data/gas.xlsx") # established path and named new .csv file in working directory
+list.files("C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data") # check that the new file now exists in the specified directory.
+dateDownloaded.gas<-date()
+dateDownloaded.gas

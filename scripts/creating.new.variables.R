c2ed76df327a8e8844e5f179ae516262ad1cbdec
Brad Presson <bradpresson@aol.com>
2025-08-25 15:57:53 -0400

    Subtle notes on the Creating New Variables lecture.

---
+++ scripts/creating.new.variables.R
@@ -0,0 +1,9 @@
+### Creating New Variables ###
+
+# Creating sequences #
+
+s1 <- seq(1,10,by=2); s1
+s2 <- seq(1,10,length=3);s2
+x <- c(1,3,8,25,100);seq(along = x) # an index that loops over the five values
+# creating sequences are useful for indexing.
+

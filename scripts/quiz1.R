e2c19dbba736e795f581119aa89f5572f72ea4b2
Brad Presson <bradpresson@aol.com>
2025-09-02 21:42:40 -0400

    Grammatical changes were made to clarify the code on line 10.

--- scripts/quiz1.R
+++ scripts/quiz1.R
@@ -7,7 +7,7 @@ if(!file.exists("data")){dir.create("data")}
 
 fileURL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv" # Idaho housing 2006 microsurvey
 setwd("C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data") # set working directory
-download.file(fileURL,destfile = "C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data/housing.csv") # established path and named new .csv file in working directory
+download.file(fileURL,destfile = "C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data/housing.csv") # download in established path and named new .csv file in working directory
 list.files("C:/Users/bradp/Documents/data.science.specialization_coursera/getting.and.cleaning.data/data") # check that the new file now exists in the specified directory.
 dateDownloaded.housing <- date() # specify when the data was downloaded. The dataset could change in the future, leading to different results.
 dateDownloaded.housing
